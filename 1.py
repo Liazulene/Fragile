105996194f4a54e85800640c32ab3c6d6d290184
Liazulene <2542130057@qq.com>
2023-04-03 11:50:05 +0800

    D

--- 1.py
+++ 1.py
@@ -1,15 +1,18 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
-x = np.linspace(0, 5, 1000)  # 在[0,5]内均匀取1000个点
-a_list = [2, 3, 4]  # 不同的a值
 
-for a in a_list:
-    y = x**(a-1) * np.exp(-x)
-    plt.plot(x, y, label=f'a = {a}')
+def g(x):
+    return math.asin(math.cos(x))
 
-plt.legend()  # 显示图例
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('x**(a-1)*exp(-x) for different a')
-plt.show()
+
+# 初始值
+x0 = 0
+
+# 迭代次数
+N = 10
+
+# 迭代
+for i in range(N):
+    x1 = g(x0)
+    print("x{} = {}".format(i+1, x1))
+    x0 = x1
