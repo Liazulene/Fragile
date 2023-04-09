b5114e53f7b1c8fd7d4cfbdce1d05dbd39bdb3c1
Liazulene <2542130057@qq.com>
2023-04-09 19:13:57 +0800

    Default

--- 1.py
+++ 1.py
@@ -1,18 +1,1 @@
 import math
-
-
-def g(x):
-    return math.asin(math.cos(x))
-
-
-# 初始值
-x0 = 0
-
-# 迭代次数
-N = 10
-
-# 迭代
-for i in range(N):
-    x1 = g(x0)
-    print("x{} = {}".format(i+1, x1))
-    x0 = x1
