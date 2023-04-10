80165fa36422101e067c4f057314f5e03a690344
Liazulene <2542130057@qq.com>
2023-04-10 10:31:58 +0800

    ----

--- Exercise/1.py
+++ Exercise/1.py
@@ -1,1 +1,31 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def dy_dt(y, t):
+    return t*y + t**3
+
+
+def ex(t):
+    return np.exp(0.5*t**2)+0.25*t**4
+
+
+y0 = 1
+
+dt = 0.01
+t = np.arange(0, 1+dt, dt)
+y2 = ex(t)
+
+y = np.zeros_like(t)
+y[0] = y0
+
+for i in range(1, len(t)):
+    y[i] = y[i-1] + dt*dy_dt(y[i-1], t[i-1])
+
+# 绘制解
+plt.plot(t, y)
+plt.plot(t, y2)
+plt.xlabel('t')
+plt.ylabel('y')
+plt.title("Solution to y'(t) = t*y + t^3, y(0) = 1")
+plt.show()
--- Exercise/3.py
+++ Exercise/3.py
@@ -1,18 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.special import gamma
 
-x = np.linspace(0, 5, 100)  # 定义x的取值范围
-z_values = [2, 3, 4]  # 定义z的取值
+a = np.array([[2, 3, 5], [7, 11, 13]])
 
-# 计算Gamma函数的值
-y_values = []
-for z in z_values:
-    y_values.append(gamma(z) * x**(z-1) * np.exp(-x))
+b = np.arange(1, np.prod(a.shape) + 1).reshape(a.shape)
 
-# 绘制曲线图
-plt.plot(x, y_values[0], label=f"z={z_values[0]}")
-plt.plot(x, y_values[1], label=f"z={z_values[1]}")
-plt.plot(x, y_values[2], label=f"z={z_values[2]}")
-plt.legend()
-plt.show()
+print(b)
