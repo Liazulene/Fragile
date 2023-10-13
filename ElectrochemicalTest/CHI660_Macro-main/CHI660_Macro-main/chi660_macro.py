7c4b3dbbd3842a786e2b7891b7e3d6cd4b825602
Liazulene <2542130057@qq.com>
2023-10-13 14:55:16 +0800

    ！

---
+++ ElectrochemicalTest/CHI660_Macro-main/CHI660_Macro-main/chi660_macro.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+
+class CHI660e(object):
+	"""docstring for CHI660e"""
+	def __init__(self, arg):
+		super(CHI660e, self).__init__()
+
+		self.arg = arg
+		self.path = os.os.getcwd()    #default file path
+
+	""" initial the lsv parameter"""
+	def init_lsv_tech(self, ):
+		pass
+
+	def run_lsv_tech():
+		pass
+
+	def function():
+		pass
+		
+
+
+doc=open('output.txt','w')
+
+print("MacroGen")
+CycleNum = 1000
+for loop in range(CycleNum)
+	print("v:"+str(0.5-loop/2000),file=doc)
+	print("run",file=doc)
+	print("save:IT-"+str(loop),file=doc)
+	print("tsave:IT-"+str(loop),file=doc)
+	print(" ",file=doc)
